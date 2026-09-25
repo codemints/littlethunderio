8ed3d194758939dd22a828041e595b5025798c6a
Daniel LittleThunder <daniel.littlethunder@moxiesozo.com>
2026-09-25 01:23:02 -0600

    Updated website name in component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="min-h-screen flex flex-col items-center justify-center">
     <h1>Hello World!</h1>
-    <h2>Welcome to burnthunder</h2>
+    <h2>Welcome to littlethunder.io</h2>
     <p>There is nothing here at the moment but you can visit <a href="https://codemints.io">codemints</a> if you're looking for something to do.</p>
     <a href="https://codemints.io" class="inline-block px-4 py-2 bg-accent text-white w-max mx-auto mt-5 hover:bg-accent/80 transition-all duration-200">VISIT CODEMINTS</a>
   </main>
